dc7111c54dd1d373748fc4c25251ed565d30a268
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-21 15:46:42 +0200

    fest: implement dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,6 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members!= undefined &&typeof members === "object" && members.length > 0) {
+    return members.filter(x => typeof x === "string").map(x => x.trim().split('')[0].toUpperCase()).sort().join('');
+  }
+  return false;
 };
