e314a26feb4b69ae7961246bdaaeeda95db63736
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-21 20:19:29 +0200

    feat: implement what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,20 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date != undefined) {
+    if (date instanceof Date && toString.call(date) === '[object Date]') {
+      let month = date.getMonth();
+      if (month >= 2 && month <= 4) {
+        return 'spring';
+      } else if (month >= 5 && month <= 7) {
+        return 'summer';
+      } else if (month >= 8 && month <= 10) {
+        return 'autumn';
+      } else if (month >= 11 || month <= 1) {
+        return 'winter';
+      }
+    } else {
+      throw Error();
+    }
+  } else {
+    return 'Unable to determine the time of year!';
+  }
 };
