76e62030276a475858c82aeedf3683d4a642ff66
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-21 14:58:35 +0200

    feat: implement carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity === "string") {
+    let activity = Number.parseFloat(sampleActivity);
+    if (activity > 0 && activity < 15) {
+      return Math.ceil((Math.log(MODERN_ACTIVITY / activity)) / (Math.log(2).toFixed(3) / HALF_LIFE_PERIOD));
+    }
+}
+return false;
 };
