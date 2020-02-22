929b1fa18ef4b28c24aa4f0ee2fbe3a0a7b58d4f
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-22 16:38:28 +0200

    feat: implement hanoi-tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,7 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    console.log('Number: ' + disksNumber + ', speed: ' + turnsSpeed);
+    return {
+        turns: Math.pow(2, disksNumber) - 1,
+        seconds: (Math.pow(2, disksNumber) - 1) / (turnsSpeed/3600)
+    }
 }
