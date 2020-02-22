da9cd240b5c490de5477c5b7fc01e2aa47621bfb
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-22 13:37:44 +0200

    feat: implement transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,35 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (Array.isArray(arr)) {
+        applyDouble(arr);
+        applyDiscard(arr);
+        return arr;
+    } else {
+        throw new Error();
+    }
 };
+
+let applyDouble = function (array) {
+    for (let i = 0; i < array.length; i++) {
+        switch (array[i]) {
+            case '--double-next':
+                if(array[i + 1] === undefined) { array.splice(i, 1); i-=1; } else { array.splice(i, 1, array[i + 1]); i+=1; }
+                break;
+            case '--double-prev':
+                if(array[i - 1] === undefined) { array.splice(i, 1); i-=1; } else { array.splice(i, 1, array[i - 1]) }
+                break;
+        }
+    }
+}
+
+let applyDiscard = function (array) {
+    for (let i = 0; i < array.length; i++) {
+        switch (array[i]) {
+            case '--discard-next':
+                if(array[i + 1] === undefined) { array.splice(i, 1); i-=1; } else { array.splice(i, 2); i-=1; }
+                break;
+            case '--discard-prev':
+                if(array[i - 1] === undefined) { array.splice(i, 1); i-=1; } else { array.splice(i - 1, 2); i-=2;}
+                break;
+        }
+    }
+}
