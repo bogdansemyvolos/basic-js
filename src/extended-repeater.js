d26a0fcd233500bec4afb7f2dc1e3904a9280aec
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-23 10:51:40 +0200

    feat: implement extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    str += repeat(
+        options.addition === null ? 'null' : options.addition,
+        options.additionRepeatTimes === undefined ? 1 : options.additionRepeatTimes,
+        options.additionSeparator === undefined ? '+' : options.additionSeparator
+    );
+    return repeat(
+        str,
+        options.repeatTimes === undefined ? 1 : options.repeatTimes,
+        options.separator === undefined ? '+' : options.separator
+    );
 };
-  
+
+let repeat = function (str, repeatCount, separator) {
+    if (str != undefined) {
+        return (str + separator).repeat(repeatCount - 1) + str;
+    } else {
+        return '';
+    }
+}
