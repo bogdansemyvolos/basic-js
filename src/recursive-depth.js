eea513c8a1ac9f043d1cf82112d0db05b3552ef2
bohdansemyvolos <b.semivolos@agilites.com>
2020-02-23 09:24:03 +0200

    feat: implement recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,17 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        if (Array.isArray(arr)) {
+            let maxDepth = 1;
+            for (let el of arr) {
+                let depth = 1;
+                depth += this.calculateDepth(el);
+                if (maxDepth < depth) {
+                    maxDepth = depth;
+                }
+            }
+            return maxDepth;
+        } else {
+            return 0;
+        }
     }
 };
